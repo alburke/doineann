75706eb06f2132790a88b3501c7f59dc8a5058df
Amanda Burke <aburke@nimbus.capsint>
2021-01-28 19:54:57 -0600

    Updated UNET3+ prediction process

--- processing/DLModeler.py
+++ processing/DLModeler.py
@@ -101,7 +101,7 @@ class DLModeler(object):
         '''
         if os.path.exists(model_file):
             del trainX,trainY,validX,validY
-            unet = tf.keras.models.load_model(model_file)
+            unet = tf.keras.models.load_model(model_file,compile=False)
             print(f'\nOpening {model_file}\n')
             #self.validate_UNET(model,validX,validY,threshold_file)
             return 
@@ -112,57 +112,16 @@ class DLModeler(object):
         #print('Validation data shape {0}'.format(np.shape(validX)))
         #print('Validation label data shape {0}\n'.format(np.shape(validY)))
         
+
+        unet3plus = models.unet_3plus_2d(np.shape(trainX[0]), n_labels=1, 
+                filter_num_down=[16, 32, 64, 128, 256], 
+                filter_num_skip='auto', filter_num_aggregate='auto', 
+                stack_num_down=2, stack_num_up=1, activation='LeakyReLU', 
+                output_activation='ReLU', batch_norm=False, pool=True, 
+                unpool=False, deep_supervision=True, name='unet3plus')
         
-        name = 'unet3plus'
-        activation = 'ReLU'
-        filter_num_down = [16, 32, 64, 128, 256]#, 512]
-        filter_num_skip = [32, 32, 32, 32]
-        filter_num_aggregate = 160
-
-        stack_num_down = 2
-        stack_num_up = 1
-        n_labels = 1
-
-        # `unet_3plus_2d_base` accepts an input tensor 
-        # and produces output tensors from different upsampling levels
-        # ---------------------------------------- #
-        input_tensor = tf.keras.layers.Input( np.shape(trainX[0]) ) 
-        # base architecture
-        X_decoder = base.unet_3plus_2d_base(
-            input_tensor, filter_num_down, filter_num_skip, filter_num_aggregate, 
-            stack_num_down=stack_num_down, stack_num_up=stack_num_up, activation=activation, 
-            batch_norm=True, pool=True, unpool=True, backbone=None, name=name)
-        
-        print(X_decoder)
-
-        # allocating deep supervision tensors
-        OUT_stack = []
-        # reverse indexing `X_decoder`, so smaller tensors have larger list indices 
-        X_decoder = X_decoder[::-1]
-
-        # deep supervision outputs
-        for i in range(1, len(X_decoder)):
-            # 3-by-3 conv2d --> upsampling --> sigmoid output activation
-            pool_size = 2**(i)
-            X = Conv2D(n_labels, 3, padding='same', name='{}_output_conv1_{}'.format(name, i-1))(X_decoder[i])
-            X = UpSampling2D((pool_size, pool_size), interpolation='bilinear', 
-                name='{}_output_sup{}'.format(name, i-1))(X)
-            X = Activation('relu', name='{}_output_sup{}_activation'.format(name, i-1))(X)
-            # collecting deep supervision tensors
-            OUT_stack.append(X)
-        print()
-        print(OUT_stack)
-        # the final output (without extra upsampling)
-        # 3-by-3 conv2d --> sigmoid output activation
-        X = Conv2D(n_labels, 3, padding='same', name='{}_output_final'.format(name))(X_decoder[0])
-        X = Activation('relu', name='{}_output_final_activation'.format(name))(X)
-        # collecting final output tensors
-        OUT_stack.append(X)
-        
-        unet3plus = tf.keras.models.Model([input_tensor,], OUT_stack)
-
-        unet3plus.compile(loss=[dice_loss,dice_loss,
-            dice_loss,dice_loss,dice_loss],
+        unet3plus.compile(loss=[
+            dice_loss,dice_loss,dice_loss,dice_loss,dice_loss],
             loss_weights=[0.25, 0.25, 0.25, 0.25, 1.0],
             optimizer=tf.keras.optimizers.Adam(lr=1e-4))
 
@@ -176,55 +135,10 @@ class DLModeler(object):
             
         #Fit UNET
         n_epochs = 2
-        bs = 400
+        bs = 500
         
         train_generator = aug.flow(trainX,trainY,batch_size=bs)
         conv_hist = unet3plus.fit(train_generator,epochs=n_epochs,verbose=1) 
-        
-        """ 
-        filters = [16, 24, 32, 48, 64]
-
-        #First layer: input shape (y,x,# variables) 
-        inputs = layers.Input(input_shape)
-        x = inputs
-        # Add noise
-        x = layers.GaussianNoise(0.01)(x)
-        
-        #Downsampling blocks 
-        c1, p1 = down_block(x, filters[0]) #128 -> 64
-        c2, p2 = down_block(p1, filters[1]) #64 -> 32
-        c3, p3 = down_block(p2, filters[2]) #32 -> 16
-        c4, p4 = down_block(p3, filters[3]) #16 -> 8
-
-        #Bottleneck
-        bn = bottleneck(p4, filters[4])
-
-        #Upsampling blocks 
-        u1 = up_block(bn, c4, filters[3]) #8 -> 16
-        u2 = up_block(u1, c3, filters[2]) #16 -> 32
-        u3 = up_block(u2, c2, filters[1]) #32 -> 64
-        u4 = up_block(u3, c1, filters[0]) #64 -> 128
-        
-        #Output layer
-        outputs = layers.Conv2D(1, (1, 1), padding='same', activation="relu")(u4)
-        
-        #Compile UNET
-        unet = models.Model(inputs, outputs)
-        unet.compile(optimizer=tf.keras.optimizers.Adam(),loss=dice_loss)
-        print(unet.summary())
-        
-        #Augment data
-        aug = tf.keras.ImageDataGenerator(
-                #rotation_range=10,zoom_range=0.15,
-                width_shift_range=0.2,height_shift_range=0.2,
-                fill_mode="nearest")
-            
-        #Fit UNET
-        n_epochs = 10
-        bs = 256
-            
-        train_generator = aug.flow(trainX,trainY,batch_size=bs)
-        conv_hist = unet.fit(train_generator,epochs=n_epochs,verbose=1) 
         
         '''
         pred_s = trainX[0].reshape(1,input_shape[0],
@@ -236,7 +150,7 @@ class DLModeler(object):
         plt.colorbar()
         plt.show()
         return
-        """
+        '''
         #Save trained model
         unet3plus.save(model_file)
         print(f'Writing out {model_file}')
@@ -315,7 +229,7 @@ class DLModeler(object):
             model.save(model_file)
             print(f'Writing out {model_file}')
         else:
-            model = models.load_model(model_file)
+            model = tf.keras.models.load_model(model_file)
             print(f'\nOpening {model_file}\n')
 
         del trainY,trainX
@@ -378,7 +292,7 @@ class DLModeler(object):
         
         #Load DL model
         model_file = self.model_path + f'/{member}_{self.model_args}_UNET.h5'
-        DL_model = models.load_model(model_file) 
+        DL_model = tf.keras.models.load_model(model_file,compile=False) 
         
         if self.model_type == 'CNN':
             #Use minimum prob threshold chosen with validation data
@@ -411,16 +325,14 @@ class DLModeler(object):
         ################## 
         # Produce gridded hourly hail forecast 
         ################## 
-        
-        #total_grid = np.zeros((standard_forecast_data.shape[0],
-        #    total_map_shape[0]*total_map_shape[1]) )
 
-        total_grid = np.zeros( (standard_forecast_data.shape[0],
-            total_map_shape[0]*total_map_shape[1]) )#*np.nan
+        total_grid = np.empty( (standard_forecast_data.shape[0],
+            total_map_shape[0]*total_map_shape[1]) )*np.nan
 
         for hour in np.arange(standard_forecast_data.shape[0]):
+            print(hour)
             #Predict probability of severe hail
-            DL_prediction = DL_model.predict(standard_forecast_data[hour])
+            DL_prediction = np.array(DL_model.predict(standard_forecast_data[hour]))
             ######
             # Will need to fix CNN code to reflect the conversion inds are in 
             #patches x (patch_radius*patch_radius) instead of (patches*radius*radius)
@@ -440,68 +352,35 @@ class DLModeler(object):
                 for patch in np.arange(standard_forecast_data.shape[1]):
                     patch_indices = patch_map_conversion_indices[patch]
                     #Gets rid of overlapping edges
-                    overlap_pts = 4
-                    total_grid[hour,patch_indices] = DL_prediction[patch,
-                        overlap_pts:-overlap_pts,overlap_pts:-overlap_pts,0].ravel()
-                    
-                    #hourly_patch_data = DL_prediction[patch,:,:,0].ravel()
-                    #total_grid[hour,patch_indices] = np.nanmean(
-                    #    (total_grid[hour,patch_indices],hourly_patch_data ),
-                    #    axis=0)
-                    '''
-                    plt.imshow(hourly_patch_data)
-                    plt.colorbar()
-                    plt.show()
-                    plt.imshow(snip_hourly_patch_data)
-                    plt.colorbar()
-                    plt.show()
-                    return 
-                    '''
-                    
-                    #if all(total_grid[hour,patch_indices]) == True:
-                    #    #if no overlapping tiles 
-                    #    total_grid[hour,patch_indices] = hourly_patch_data
-                    #else: 
-                        #total_grid[hour,patch_indices] = np.nanmean(
-                        #total_grid[hour,patch_indices] = gaussian_filter(combined_data,32)
-            #Smooth edge values
-
-            #total_grid[hour] = gaussian_filter(total_grid[hour],14)
-            #total_grid[hour][total_grid[hour] <= 0.1] = 0.0
-        del standard_forecast_data
+                    overlap_pt = 4
+                    # If unet3+ then the last output tensor is the correct one
+                    hourly_patch_data = DL_prediction[-1,patch,overlap_pt:-overlap_pt,
+                        overlap_pt:-overlap_pt,0].ravel()
+                    total_grid[hour,patch_indices] = hourly_patch_data
         del DL_prediction
+        del standard_forecast_data
         output_data=total_grid.reshape((total_grid.shape[0],)+total_map_shape)
         
         date_outpath = forecast_grid_path + f'{date[0][:-5]}/'
+        
         #Output gridded forecasts
         if not os.path.exists(date_outpath): os.makedirs(date_outpath)
         gridded_out_file = date_outpath + f'{member}_{date[0]}_forecast_grid.h5'
         print(f'Writing out {gridded_out_file}')
-
-        with h5py.File(gridded_out_file, 'w') as hf: hf.create_dataset("data",
-            output_data,compression='gzip',compression_opts=6)
+        with h5py.File(gridded_out_file, 'w') as hf: 
+            hf.create_dataset("data",data=output_data,
+            compression='gzip',compression_opts=6)
         
+        '''
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1,projection = ccrs.LambertConformal())
         ax.add_feature(cf.COASTLINE)   
         ax.add_feature(cf.OCEAN)
         ax.add_feature(cf.BORDERS,linestyle='-')
         ax.add_feature(cf.STATES.with_scale('50m'),linestyle='-',edgecolor='black')
-        plt.contourf(lon_grid,lat_grid,output_data[0],transform=ccrs.PlateCarree())
-        plt.colorbar()
+        plt.contourf(lon_grid,lat_grid,output_data[0,:,:],transform=ccrs.PlateCarree())
         plt.show()
-
-        fig = plt.figure()
-        ax = fig.add_subplot(1,1,1,projection = ccrs.LambertConformal())
-        ax.add_feature(cf.COASTLINE)   
-        ax.add_feature(cf.OCEAN)
-        ax.add_feature(cf.BORDERS,linestyle='-')
-        ax.add_feature(cf.STATES.with_scale('50m'),linestyle='-',edgecolor='black')
-        smooth=gaussian_filter(output_data[0],32) 
-        plt.contourf(lon_grid,lat_grid,smooth,transform=ccrs.PlateCarree())
-        plt.colorbar()
-        plt.show()
-        
+        '''
         return
 
 def dice_loss(y_true, y_pred):
